95dcd6b4b96a18ee769437cb50a08cdfc18c4191
allenwuccu <90852184+allenwuccu@users.noreply.github.com>
2023-06-02 23:00:37 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+using namespace std;
+int main(void){
+    int height = 0, gender = 0;
+    float weight = 0;
+    while(cin >> height >> gender && height != EOF){
+        if(gender == 1){
+            weight = (height - 80) * 0.7;
+            cout  << fixed  <<  setprecision(1) << weight <<  endl;
+            //printf("%.1f\n", weight);
+        }
+        if(gender == 2){
+            weight = (height - 70) * 0.6;
+            cout  << fixed  <<  setprecision(1) << weight <<  endl;
+        }
+    }
+    return 0;
+}
